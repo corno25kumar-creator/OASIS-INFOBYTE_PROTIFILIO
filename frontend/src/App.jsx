cf84811612e509a4daa92cfc0030201c99d2c04c
Corno Kumar <corno25kumar@gmail.com>
2025-12-03 16:43:04 +0530

    add hero and skill section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,57 @@
+// import { useState, useEffect } from "react";
+// import PreLoader from "./components/PreLoader/preLoader";
+// import HeroPage from "./components/hero/heroPage";
+
+// import SkillPage from "./components/skill/skillPage";
+// function App() {
+//   const [loadingDone, setLoadingDone] = useState(false);
+
+//   useEffect(() => {
+//     // Preloader runs for ~7.5 sec (your GSAP delay)
+//     const timer = setTimeout(() => {
+//       setLoadingDone(true);
+//     }, 7600); // match your animation timing
+
+//     return () => clearTimeout(timer);
+//   }, []);
+
+//   return (
+//     <div>
+//       {!loadingDone ? <PreLoader /> : <HeroPage />}
+   
+//     <SkillPage/>
+//     </div>
+//   );
+// }
+
+// export default App;
 import { useState, useEffect } from "react";
 import PreLoader from "./components/PreLoader/preLoader";
 import HeroPage from "./components/hero/heroPage";
+import SkillPage from "./components/skill/skillPage";
 
 function App() {
   const [loadingDone, setLoadingDone] = useState(false);
 
   useEffect(() => {
-    // Preloader runs for ~7.5 sec (your GSAP delay)
     const timer = setTimeout(() => {
       setLoadingDone(true);
-    }, 7600); // match your animation timing
+    }, 7800);
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <div>
-      {!loadingDone ? <PreLoader /> : <HeroPage />}
-    </div>
+    <>
+      {!loadingDone ? (
+        <PreLoader />
+      ) : (
+        <>
+         <HeroPage/>
+          <SkillPage />
+        </>
+      )}
+    </>
   );
 }
 
